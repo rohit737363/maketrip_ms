e2ddbae37d7406730768cf59ed7dcf7796918419
rohit737363 <chougalerohit73@gmail.com>
2023-03-07 18:29:59 +0530

    upload mmt application

---
+++ src/main/java/com/example/maketrip/MaketripApplication.java
@@ -0,0 +1,13 @@
+package com.example.maketrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaketripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaketripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maketrip/MaketripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maketrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaketripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
